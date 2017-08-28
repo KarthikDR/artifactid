cafe29b3edf155cb1a2c7d20e3a4cdc07e027601
Karthik Donthy <kdonthy@vmware.com>
2017-08-28 17:18:35 +0530

    added test for AppSum Class

---
+++ src/test/java/groupid/AppSumTest.java
@@ -0,0 +1,16 @@
+package groupid;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppSumTest {
+
+	@Test
+	public void test() {
+		
+		assertEquals(AppSum.sum(10, 20),30);
+
+	}
+
+}
